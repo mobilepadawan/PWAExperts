8ac4d7eda5b9dc069d8fdf90fa893ee431670389
Fernando Luna <fernando@vidamobile.com.ar>
2021-01-13 08:51:57 -0300

    Delete tasks items
    I abled the delete of tasks items

---
+++ Wake-Lock-Sample/js/wla.js
@@ -0,0 +1,39 @@
+let mensaje = ""
+let pausado = false
+const lock = document.getElementById("lock")
+let wakeLock = null;
+
+lock.addEventListener("click", () => {
+    if ('wakeLock' in navigator) {
+        if (pausado) { liberoAutoBloqueo() } else { pausoAutoBloqueo() }
+    } else {
+        mensaje = "Su navegador no soporta Wake Lock API"
+        mostrarMensaje()
+    }
+})
+
+function pausoAutoBloqueo() {
+        try {
+          wakeLock = navigator.wakeLock.request("screen");
+          mensaje = "Se ha pausado el autobloqueo de pantalla."
+          lock.src = "images/locked.png"
+          pausado = true
+        } catch (err) {
+            mensaje = `Error: ${err.message}`
+            pausado = false
+        }
+        mostrarMensaje()
+}
+
+function liberoAutoBloqueo() {
+    // wakeLock.release();
+    wakeLock = null;
+    mensaje = "Se activó el autobloqueo de pantalla."
+    lock.src = "images/unlocked.png"
+    pausado = false
+    mostrarMensaje()
+}
+
+function mostrarMensaje() {
+    M.toast({html: mensaje, displayLength: 2000})
+}
